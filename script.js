da184adc619f20cd7c177f84724e84420e7c137a
GeisianeAlves <geisiane-alves2011@hotmail.com>
2022-04-27 08:55:25 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+function changeMode() {
+	changeClasses();
+	changeText();
+}
+
+function changeClasses() {
+	button.classList.toggle(darkModeClass);
+	h1.classList.toggle(darkModeClass);
+	body.classList.toggle(darkModeClass);
+	footer.classList.toggle(darkModeClass);
+}
+
+function changeText() {
+	const lightMode = 'Light Mode';
+	const darkMode = 'Dark Mode';
+
+	if (body.classList.contains(darkModeClass)) {
+		button.innerHTML = lightMode;
+		h1.innerHTML = darkMode + ' ON';
+		return;
+	}
+
+	button.innerHTML = darkMode;
+	h1.innerHTML = lightMode + ' ON';
+}
+
+const darkModeClass = 'dark-mode';
+const button = document.getElementById('mode-selector');
+const h1 = document.getElementById('page-title');
+const body = document.getElementsByTagName('body')[0];
+const footer = document.getElementsByTagName('footer')[0];
+
+/*
+const div = document.getElementsById('box');
+const divF = document.getElementById('right-content');
+const divF2 = document.getElementById('botao');*/
+
+button.addEventListener('click', changeMode);
